cc805004c818567a8b42d4f3f9b4d213bfa56371
fenglong_ng <fenglong1502@hotmail.com>
2023-06-11 23:38:28 +0800

    Setup project folder with header and footer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import * as ROUTES from './constants/routes';
+
+import HomePage from './pages/home';
+import Layout from './components/layout/Layout';
+import ProjectsPage from './pages/projects/Projects';
+import AboutPage from './pages/aboutus/About';
+import BlogPage from './pages/blog/Blog';
+import ResourcesPage from './pages/resources/Resources';
+import ContactPage from './pages/contact/Contact';
 
 function App() {
-  return <div className="App">Application page</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path={ROUTES.HOME} element={<HomePage />} />
+          <Route path={ROUTES.ABOUT} element={<AboutPage />} />
+          <Route path={ROUTES.PROJECTS} element={<ProjectsPage />} />
+          <Route path={ROUTES.BLOG} element={<BlogPage />} />
+          <Route path={ROUTES.RESOURCES} element={<ResourcesPage />} />
+          <Route path={ROUTES.CONTACT} element={<ContactPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
