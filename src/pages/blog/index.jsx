f06eaf368e13ccea45474ea476896a1556f568ae
fenglong_ng <fenglong1502@hotmail.com>
2023-06-30 07:56:16 +0800

    Connect to notion

--- src/pages/blog/index.jsx
+++ src/pages/blog/index.jsx
@@ -1,15 +1,45 @@
-import Container from "../../components/container/Container";
+import { useEffect } from 'react';
+import Container from '../../components/container/Container';
+import { Client } from '@notionhq/client';
 
-const BlogPage = () => {
-    return (
-        <>
-          <Container className="mt-9">
-            <div className="max-w-2xl">
-              <div>Blog Page</div>
-            </div>
-          </Container>
-        </>
-      );
+const BlogPage = (articles) => {
+  return (
+    <>
+      <Container className="mt-9">
+        <div className="max-w-2xl">
+          <div>Blog Page</div>
+        </div>
+      </Container>
+    </>
+  );
 };
+
+export async function getStaticProps() {
+  const notionDatabaseId = process.env.REACT_APP_NOTION_DATABASE;
+  const notionSecret = process.env.REACT_APP_NOTION_API_KEY;
+
+  if (!notionDatabaseId || !notionSecret) {
+    return {
+      notFound: true,
+    };
+  }
+
+  const notion = new Client({
+    auth: notionSecret,
+  });
+
+  const query = await notion.databases.query({
+    database_id: notionDatabaseId,
+    // sort by the most recently created posts
+    sorts: [{ property: 'Created', direction: 'descending' }],
+    filter: { property: 'Public', checkbox: { equals: true } },
+  });
+
+  return {
+    props: {
+      articles: query?.results ?? [],
+    },
+  };
+}
 
 export default BlogPage;
