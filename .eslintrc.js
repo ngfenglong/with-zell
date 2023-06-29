81e988e72448be23d519c31958bc7304f39ddc87
fenglong_ng <fenglong1502@hotmail.com>
2023-06-29 13:40:55 +0800

    add eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    root: true,
+    env: {
+      browser: true,
+      node: true,
+    },
+    extends: ['eslint:recommended', 'plugin:react/recommended', 'next'],
+    rules: {
+      // Add any additional ESLint rules or overrides here
+    },
+  };
+  
