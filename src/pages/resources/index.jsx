89bcafd254b27bb5ed4b467d3af5e210a5aa3e60
Zell <fenglong1502@hotmail.com>
2024-02-20 08:44:15 +0800

    Add Resource Page

--- src/pages/resources/index.jsx
+++ src/pages/resources/index.jsx
@@ -1,34 +1,48 @@
-import Link from 'next/link';
-import Container from '../../components/container/Container';
+import Container from "../../components/container/Container";
+import { getBlocks, getPage } from "@/util/notion";
+import { Fragment } from "react";
+import { renderBlock } from "@/util/notion-rendering-helper";
 
-const ResourcesPage = () => {
+const ResourcesPage = ({page, blocks, dateStr}) => {
   return (
     <>
       <Container className="pb-12 pt-16">
-        <main className="mx-auto flex w-full max-w-7xl flex-grow flex-col justify-center px-6 lg:px-8">
-          <div className="py-16">
-            <div className="text-center">
-              <h1 className="mt-2 text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">
-                Oops
-              </h1>
-              <p className="mt-2 text-base text-gray-500">
-                This resource page is currently a work in progress.
-              </p>
-              <div className="mt-6">
-                <Link
-                  href="/"
-                  className="text-base font-medium text-teal-600 hover:text-teal-500"
-                >
-                  Go back home
-                  <span aria-hidden="true"> &rarr;</span>
-                </Link>
-              </div>
-            </div>
-          </div>
-        </main>
+        <article>
+          <section>
+            {blocks.map((block) => (
+              <Fragment key={block.id}>{renderBlock(block)}</Fragment>
+            ))}
+          </section>
+        </article>
       </Container>
     </>
   );
+};
+
+export const getServerSideProps = async () => {
+  const notionSecret = process.env.NOTION_API_KEY;
+  const id = process.env.RESOURCE_PAGE_ID;
+
+  if (!notionSecret || !id) {
+    return {
+      notFound: true,
+    };
+  }
+
+  const page = await getPage(id);
+  const blocks = await getBlocks(id);
+
+  const created_time = page.created_time;
+  const releasedDate = page.properties?.released_date?.date?.start;
+  const dateStr = releasedDate ?? created_time;
+
+  return {
+    props: {
+      page,
+      blocks,
+      dateStr,
+    },
+  };
 };
 
 export default ResourcesPage;
